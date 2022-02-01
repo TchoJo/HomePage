08bf19965c685c81f8eb455d480c1c9b2f737458
ROY <zroy2000@gmail.com>
2022-02-01 16:22:34 +0200

    update !

--- pomodorTimer.js
+++ pomodorTimer.js
@@ -8,7 +8,7 @@ const stopper = document.getElementById("stopTheEngine");
 const shortBreaker = document.getElementById("shortBreak");
 const longBreaker = document.getElementById("longBreak");
 
-let set = 0;
+let set = null;
 
 //  functions
 startPomo = () => pomodoroTimer(25 * 60); //25 Minutes 
@@ -16,13 +16,19 @@ shortBreakPomo = () => pomodoroTimer(5 * 60);//5 Minutes
 longBreakPomo = () =>  pomodoroTimer(15 * 60);//15 Minutes
 showStopTime = () => {
   clearInterval(set);
+  set = null;
 }
   pomodoroTimer = (seconds) => {
-    clearInterval(set)
+//    clearInterval(set)
     const now = Date.now();
+    if(!Date.now()) {
+      Date.now = () => {
+        return new Date().getTime(); 
+      }
+    }
     const before = now + seconds * 1000;
+  
     showTime(seconds);
-
     set = setInterval(() => {
     const timeLeft = Math.round((before - Date.now()) / 1000);
     if (timeLeft < 0 ) {
